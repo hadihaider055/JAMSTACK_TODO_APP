6f5ccb7b5d781c524ab1318a9d2a7a7f19412d04
hadihaider055 <haiderhadi055@gmail.com>
2021-11-04 15:04:38 +0500

    first commit

---
+++ functions/todos/todos.js
@@ -0,0 +1,87 @@
+const { ApolloServer, gql } = require("apollo-server-lambda");
+var faunadb = require("faunadb"),
+  q = faunadb.query;
+
+const typeDefs = gql`
+  type Query {
+    todos: [Todo!]
+  }
+  type Mutation {
+    addTodo(task: String!): Todo
+    isDone(id: ID!, isCompleted: Boolean!): Todo
+  }
+  type Todo {
+    id: ID!
+    task: String!
+    isCompleted: Boolean!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    todos: async (root, args, context) => {
+      try {
+        var adminClient = new faunadb.Client({
+          secret: process.env.FAUNADB_SERVER_SECRET,
+        });
+        const result = await adminClient.query(
+          q.Map(
+            q.Paginate(q.Match(q.Index("todos"))),
+            q.Lambda((x) => q.Get(x))
+          )
+        );
+        return result.data.map((item) => {
+          return {
+            id: item.ref.id,
+            task: item.data.task,
+            isCompleted: item.data.isCompleted,
+          };
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  Mutation: {
+    addTodo: async (_, { task }) => {
+      try {
+        var adminClient = new faunadb.Client({
+          secret: process.env.FAUNADB_SERVER_SECRET,
+        });
+        const result = await adminClient.query(
+          q.Create(q.Collection("todos"), {
+            data: {
+              task: task,
+              isCompleted: false,
+            },
+          })
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    isDone: async (_, { id, isCompleted }) => {
+      try {
+        var adminClient = new faunadb.Client({
+          secret: process.env.FAUNADB_SERVER_SECRET,
+        });
+        const result = await adminClient.query(
+          q.Update(q.Ref(q.Collection("todos"), id), {
+            data: {
+              isCompleted: !isCompleted,
+            },
+          })
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+exports.handler = server.createHandler();
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,129 @@
-import * as React from "react"
+import React from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import {
+  IoCheckmarkDoneCircleSharp,
+  IoCheckmarkDoneCircleOutline,
+} from "react-icons/io5";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const Home = () => {
+  let inputText;
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+  const [addTodo] = useMutation(ADD_TODO);
+  const [isDone] = useMutation(MARK_AS_DONE);
+  const addTask = (e) => {
+    e.preventDefault();
+    addTodo({
+      variables: {
+        task: inputText.value,
+      },
+      refetchQueries: [{ query: GET_TODOS }],
+    });
+    inputText.value = "";
+  };
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+  const handleDone = (todo) => {
+    isDone({
+      variables: {
+        id: todo.id,
+        isCompleted: todo.isCompleted,
+      },
+      refetchQueries: [{ query: GET_TODOS }],
+    });
+  };
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+  const { loading, error, data } = useQuery(GET_TODOS);
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
+  if (loading)
+    return (
+      <div className="min-h-screen h-full absolute top-2/4 left-2/4 -mt-10 -ml-10">
+        <h2 className="text-2xl text-center font-semibold">Loading..</h2>
+      </div>
+    );
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+  if (error)
+    return (
+      <div className="min-h-screen h-full absolute top-2/4 left-2/4 -mt-10 -ml-10">
+        <h2 className="text-2xl text-center font-semibold">
+          Oops, Something went wrong! <br /> Please Try Again Later!..
+        </h2>
+      </div>
+    );
 
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
-const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+    <div className="mx-auto max-w-7xl w-full min-h-screen h-full">
+      <div className="flex items-center justify-center">
+        <div className="absolute top-2/4 -mt-32">
+          <h2 className="text-2xl text-center font-semibold mb-5">Todo App</h2>
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              ref={(node) => {
+                inputText = node;
+              }}
+              required
+              className="border border-gray-700 block p-2 w-72 rounded-sm placeholder-black  focus:outline-none focus:ring-2 bg-transparent focus:ring-indigo-700 focus:border-transparent shadow-md"
+              placeholder="Enter task to do.."
+            />
+            <button className="bg-indigo-700 text-white mt-2 w-72 h-10 rounded-sm hover:bg-indigo-800 focus:outline-none focus:ring-2 active:bg-indigo-800 focus:ring-indigo-700 focus:ring-offset-2 shadow-lg">
+              Add Todo
+            </button>
+          </form>
+          <div className="mt-16">
+            <h2 className="text-2xl text-center font-semibold">Todo List</h2>
+            <ul>
+              {data.todos.map((todo) => (
+                <li
+                  key={todo.id}
+                  className="mt-3 h-15 border shadow-sm hover:shadow-md cursor-pointer transition-all duration-500 ease-in-out p-3 flex items-center justify-between"
+                >
+                  <p className={`${todo.isCompleted ? "line-through" : ""}`}>
+                    {todo.task}
+                  </p>
+                  <p>
+                    {todo.isCompleted ? (
+                      <IoCheckmarkDoneCircleSharp
+                        onClick={() => handleDone(todo)}
+                      />
+                    ) : (
+                      <IoCheckmarkDoneCircleOutline
+                        onClick={() => handleDone(todo)}
+                      />
+                    )}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default IndexPage
+export default Home;
+
+const GET_TODOS = gql`
+  {
+    todos {
+      task
+      id
+      isCompleted
+    }
+  }
+`;
+const ADD_TODO = gql`
+  mutation addTodo($task: String!) {
+    addTodo(task: $task) {
+      task
+    }
+  }
+`;
+const MARK_AS_DONE = gql`
+  mutation isDone($id: ID!, $isCompleted: Boolean!) {
+    isDone(id: $id, isCompleted: $isCompleted) {
+      id
+      isCompleted
+    }
+  }
+`;
